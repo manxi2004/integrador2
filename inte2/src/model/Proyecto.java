8251016505ebfe0fc55a94742da6aec931625002
Jose Manuel Cardona Rondón <124000426+manxi2004@users.noreply.github.com>
2023-04-23 00:17:52 -0500

    Update Proyecto.java

--- inte2/src/model/Proyecto.java
+++ inte2/src/model/Proyecto.java
@@ -1,82 +1,56 @@
 package model;
 
+import java.util.Date;
+
 public class Proyecto {
-    private static int contadorEtapa = 0;
-    private static int contadorCapsula = 0;
+    private String name;
+    private String nombreDelCliente;
+    private Date fechaDeInicio;
+    private Date fechaDeFinal;
+    private Double presupuesto;
+    private String nombreGerente;
+    private int numeroCelular;
 
-    private String nombre;
-    private Gerente gerente;
-    private Etapa[] etapas;
-    private Capsula[] capsulas;
+    public Proyecto(String name, String nombreDelCiente, Date fechaDeInicio, Date fechaDeFin, Double presupuesto,
+            String nombreGerente, int numeroCelular) {
 
-    public Proyecto(String nombre, Gerente gerente) {
-        this.nombre = nombre;
-        this.gerente = gerente;
-        this.etapas = new Etapa[10];
-        this.capsulas = new Capsula[100];
+        this.name = name;
+        this.nombreDelCliente = nombreDelCiente;
+        this.fechaDeInicio = fechaDeInicio;
+        this.fechaDeFinal = fechaDeFin;
+        this.presupuesto = presupuesto;
+        this.nombreGerente = nombreGerente;
+        this.numeroCelular = numeroCelular;
+
     }
 
-    public Etapa crearEtapa(String nombre) {
-        Etapa etapa = new Etapa(contadorEtapa++, nombre);
-        etapas[etapa.getId()] = etapa;
-        return etapa;
+    public String getName() {
+
+        return name;
+
     }
 
-    public boolean culminarEtapa(int id) {
-        Etapa etapa = buscarEtapa(id);
-        if (etapa == null) {
-            return false;
-        }
-        etapa.culminar();
-        return true;
+    public String getNombreDelCliente() {
+
+        return nombreDelCliente;
+
     }
 
-    public boolean registrarCapsula(Capsula capsula, int idEtapa) {
-        Etapa etapa = buscarEtapa(idEtapa);
-        if (etapa == null) {
-            return false;
-        }
-        capsula.setId(contadorCapsula++);
-        etapa.agregarCapsula(capsula);
-        capsulas[capsula.getId()] = capsula;
-        return true;
+    public Date getFechaDeInicio() {
+
+        return fechaDeInicio;
+
     }
 
-    public boolean aprobarCapsula(int id) {
-        Capsula capsula = buscarCapsula(id);
-        if (capsula == null) {
-            return false;
-        }
-        gerente.aprobarCapsula(this, capsula.getId());
-        return true;
+    public Date getFechaDeFin() {
+
+        return fechaDeFinal;
+
     }
 
-    public boolean publicarCapsula(int id) {
-        Capsula capsula = buscarCapsula(id);
-        if (capsula == null) {
-            return false;
-        }
-        gerente.publicarCapsula(this, capsula.getId());
-        return true;
+    public Double getPresupuesto() {
+
+        return presupuesto;
     }
 
-    private Etapa buscarEtapa(int id) {
-        if (id >= etapas.length || etapas[id] == null) {
-            return null;
-        }
-        return etapas[id];
-    }
-
-    public Capsula buscarCapsula(int id) {
-        for (Capsula capsula : capsulas) {
-            if (capsula != null && capsula.getId() == id) {
-                return capsula;
-            }
-        }
-        return null;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
 }
