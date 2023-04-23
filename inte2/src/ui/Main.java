c91a6d57ce21248cb85a3d8d3f12b1aeada85f0e
Jose Manuel Cardona Rondón <124000426+manxi2004@users.noreply.github.com>
2023-04-23 00:10:49 -0500

    cambios clase Main

--- inte2/src/ui/Main.java
+++ inte2/src/ui/Main.java
@@ -1,98 +1,85 @@
 package ui;
 
-import model.Gerente;
-import model.Capsula;
-import model.Etapa;
-import model.Proyecto;
+import model.GreenSQA;
+
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Gerente gerente = new Gerente("Juan Perez");
-        Proyecto proyecto = new Proyecto("Proyecto de software", gerente);
+    private Scanner reader;
+    private GreenSQA controller;
 
+    public Main() {
+        this.reader = new Scanner(System.in);
+        this.controller = new GreenSQA();
+    }
+
+    public void iniciar() throws ParseException {
         int opcion;
+
         do {
-            System.out.println("\n---- Menú ----");
-            System.out.println("1. Crear etapa");
-            System.out.println("2. Culminar etapa");
-            System.out.println("3. Registrar cápsula");
-            System.out.println("4. Aprobar cápsula");
-            System.out.println("5. Publicar cápsula");
-            System.out.println("6. Salir");
-            System.out.print("Ingrese una opción: ");
-            opcion = scanner.nextInt();
-            scanner.nextLine(); // Limpiar el buffer de entrada
+            System.out.println("¿Qué desea hacer?");
+            System.out.println("1. Registrar un projecto");
+            System.out.println("2. culminar una etapa");
+            System.out.println("registrar una capsula");
+            System.out.println("4.aprobar una capsula ");
+            System.out.println("5.ver numero de capsulas por tipo");
+            System.out.println("6.ver si un colaborador ha añadido capsulas");
+            System.out.println("7.informar situacion por #");
+            System.out.println("8.salir");
+
+            opcion = reader.nextInt();
 
             switch (opcion) {
                 case 1:
-                    System.out.print("Ingrese el nombre de la etapa: ");
-                    String nombreEtapa = scanner.nextLine();
-                    Etapa etapa = proyecto.crearEtapa(nombreEtapa);
-                    System.out.println("Etapa creada con éxito. ID de la etapa: " + etapa.getId());
+
+                    controller.registrarProyecto();
+
                     break;
                 case 2:
-                    System.out.print("Ingrese el ID de la etapa a culminar: ");
-                    int idEtapa = scanner.nextInt();
-                    scanner.nextLine(); // Limpiar el buffer de entrada
-                    boolean etapaCulminada = proyecto.culminarEtapa(idEtapa);
-                    if (etapaCulminada) {
-                        System.out.println("Etapa culminada con éxito");
-                    } else {
-                        System.out.println("No se encontró ninguna etapa con ese ID");
-                    }
+                    controller.culminarEtapa();
                     break;
                 case 3:
-                    System.out.print("Ingrese el nombre de la cápsula: ");
-                    String nombreCapsula = scanner.nextLine();
-                    System.out.print("Ingrese la descripción de la cápsula: ");
-                    String descripcionCapsula = scanner.nextLine();
-                    System.out.print("Ingrese el ID de la etapa: ");
-                    int idEtapaCapsula = scanner.nextInt();
-                    scanner.nextLine(); // Limpiar el buffer de entrada
-                    Capsula capsula = new Capsula(idEtapaCapsula, nombreCapsula, descripcionCapsula);
-                    boolean capsulaRegistrada = proyecto.registrarCapsula(capsula, idEtapaCapsula);
-                    if (capsulaRegistrada = true) {
-                        System.out.println("Cápsula registrada con éxito. ID de la cápsula: " + capsula.getId());
-                    } else {
-                        System.out.println("No se encontró ninguna etapa con ese ID");
-                    }
+                    controller.registrarCapsula();
+
                     break;
 
                 case 4:
-                    System.out.print("Ingrese el ID de la cápsula a aprobar: ");
-                    int idCapsula = scanner.nextInt();
-                    scanner.nextLine(); // Limpiar el buffer de entrada
-                    boolean capsulaAprobada = gerente.aprobarCapsula(proyecto, idCapsula);
-                    if (capsulaAprobada = true) {
-                        System.out.println("Cápsula aprobada con éxito");
-                    } else {
-                        System.out
-                                .println("No se encontró ninguna cápsula con ese ID o la cápsula ya ha sido aprobada");
-                    }
-                    break;
-                case 5:
-                    System.out.print("Ingrese el ID de la cápsula a publicar: ");
-                    int idCapsulaPublicar = scanner.nextInt();
-                    scanner.nextLine(); // Limpiar el buffer de entrada
-                    boolean capsulaPublicada = gerente.publicarCapsula(proyecto, idCapsulaPublicar);
-                    if (capsulaPublicada = true) {
-                        System.out.println("Cápsula publicada con éxito");
-                    } else {
-                        System.out
-                                .println("No se encontró ninguna cápsula con ese ID o la cápsula no ha sido aprobada");
-                    }
-                    break;
-                case 6:
-                    System.out.println("Saliendo del programa...");
-                    break;
-                default:
-                    System.out.println("Opción inválida. Intente de nuevo");
-                    break;
-            }
+                    controller.aprobarCapsula();
 
-        } while (opcion != 6);
+                    break;
+
+                case 5:
+
+                    controller.contarTiposDeCapsula();
+
+                    break;
+
+                case 6:
+
+                    controller.capsulasColaborador();
+
+                    break;
+
+                case 7:
+
+                    controller.buscarCapsula();
+
+                    break;
+
+                case 8:
+
+                    System.out.println("adios");
+
+                default:
+                    System.out.println("Opción inválida. Por favor, elija una opción válida.");
+            }
+        } while (opcion != 8);
+    }
+
+    public static void main(String[] args) throws ParseException {
+        Main programa = new Main();
+        programa.iniciar();
     }
 }
