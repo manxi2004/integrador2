716a3d53a6876045b18c2d9b49e4b2c16c466868
Jose Manuel Cardona Rondón <124000426+manxi2004@users.noreply.github.com>
2023-04-23 00:15:33 -0500

    Update Etapa.java

--- inte2/src/model/Etapa.java
+++ inte2/src/model/Etapa.java
@@ -1,63 +1,21 @@
 package model;
 
+import java.util.Date;
+
 public class Etapa {
-    private static int ID_GLOBAL = 1;
-    private int id;
-    private String nombre;
-    private boolean culminada;
-    private Capsula[] capsulas;
-    private int cantidadCapsulas;
 
-    public Etapa(int contador, String nombre) {
-        this.id = ID_GLOBAL;
-        ID_GLOBAL++;
-        this.nombre = nombre;
-        this.culminada = false;
-        this.capsulas = new Capsula[10];
-        this.cantidadCapsulas = 0;
+    private Date fechaDeInicioReal;
+    private Date fechaDeFinalReal;
+    private boolean aprobada;
+    private boolean estado;
+
+    public Etapa(Date fechaDeInicioReal, Date fechaDeFinalReal, boolean aprobada, boolean estado) {
+
+        this.fechaDeInicioReal = fechaDeInicioReal;
+        this.fechaDeFinalReal = fechaDeFinalReal;
+        this.aprobada = aprobada;
+        this.estado = estado;
+
     }
 
-    public boolean registrarCapsula(Capsula capsula) {
-        if (culminada) {
-            return false;
-        }
-        if (cantidadCapsulas >= 10) {
-            return false;
-        }
-        capsulas[cantidadCapsulas] = capsula;
-        cantidadCapsulas++;
-        return true;
-    }
-
-    public void culminar() {
-        culminada = true;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public Capsula[] getCapsulas() {
-        return capsulas;
-    }
-
-    public boolean agregarCapsula(Capsula capsula) {
-        if (culminada) {
-            return false;
-        }
-        if (cantidadCapsulas >= 10) {
-            return false;
-        }
-        boolean capsulaAgregada = false;
-        for (int i = 0; i < capsulas.length; i++) {
-            if (capsulas[i] == null) {
-                capsulas[i] = capsula;
-                cantidadCapsulas++;
-                capsulaAgregada = true;
-                break;
-            }
-        }
-        return capsulaAgregada;
-    }
-    
 }
